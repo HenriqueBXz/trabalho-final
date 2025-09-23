f7d1296c105a6d36748e086893316ffe48f3961b
Joaoaleluia <joaoaleluia7246ce@gmail.com>
2025-09-23 09:44:00 -0300

    Add files via upload

---
+++ home/script.js
@@ -0,0 +1,10 @@
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+
+    toggleButton.addEventListener("click", function () {
+      menu.classList.toggle("show");
+    });
+  });
+</script>
